d54ee85f6b2cb7e593b3c8fb9251e1cde2f0820d
Andreea <andreea.celmare@gmail.com>
2024-10-17 17:39:44 +0200

    feat(JS): Slicing and Extracting Parts of a String

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ console.log("Hello" + " John");
 console.log(introMessage + " there ", userName);
 console.log(userName.length);
 
+// String Lengths and Retrieving the Number of Characters
 // Write a concatenation that gives the result: You have written 182 characters, you have -42 characters left.
 
-var tweet = prompt("Compose your tweet");
+/* var tweet = prompt("Compose your tweet");
 var tweetCount = tweet.length;
 console.log(
   "You have written " +
@@ -14,4 +15,17 @@ console.log(
     " characters, you have " +
     (140 - tweetCount) +
     " characters left."
-);
+); 
+
+var tweetUnder140 = tweet.slice(0,140);
+alert(tweetUnder140);
+
+
+alert(prompt("Compose your Tweet:").slice(0,140));
+
+*/
+
+// Slicing and Extracting Parts of a String
+/* var secondName = "Angela";
+secondName.slice(0, 2); 
+*/
