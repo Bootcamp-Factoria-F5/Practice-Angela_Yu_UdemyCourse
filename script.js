c0f7d2bf776b896289b16345487a98ffe71fceb2
Andreea <andreea.celmare@gmail.com>
2024-10-17 17:24:28 +0200

    feat(Java): String Lengths and Retrieving the Number of Characters

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 var introMessage = "Hello";
 var userName = "John";
-alert("Hello" + " John");
+console.log("Hello" + " John");
+console.log(introMessage + " there ", userName);
+console.log(userName.length);
+
+// Write a concatenation that gives the result: You have written 182 characters, you have -42 characters left.
+
+var tweet = prompt("Compose your tweet");
+var tweetCount = tweet.length;
+console.log(
+  "You have written " +
+    tweetCount +
+    " characters, you have " +
+    (140 - tweetCount) +
+    " characters left."
+);
